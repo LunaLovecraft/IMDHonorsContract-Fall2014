73b5dc170cbd618251f5df8928eedf1f0c3aacfe
LunaLovecraft <lunalovecraft@gmail.com>
2014-10-12 13:46:11 -0400

    Implemented Spikes Partially
    Setup the base code, just needs a player class to have a kill method for
    it to be finished.

---
+++ SpiderWeb/Assets/Scripts/TriggerZone Scripts/AbstractTriggerZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractTriggerZone : MonoBehaviour {
+
+    /// <summary>
+    /// When something enters the plane the following code triggers.
+    /// </summary>
+    /// <param name="other">The object in the trigger zone.</param>
+    public abstract void OnTriggerEnter(Collider other);
+
+    /// <summary>
+    /// While the object in the plane is in the plane the following code runs.
+    /// </summary>
+    /// <param name="other">The object in the trigger zone.</param>
+    public abstract void OnTriggerStay(Collider other);
+
+    /// <summary>
+    /// When the object leaves the plane the following code runs.
+    /// </summary>
+    /// <param name="other">The object in the trigger zone.</param>
+    public abstract void OnTriggerExit(Collider other);
+
+}
---
+++ SpiderWeb/Assets/Scripts/TriggerZone Scripts/ExampleTriggerZoneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleTriggerZoneScript : AbstractTriggerZone {
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.ToString() + " has entered trigger zone " + this.name);
+    }
+
+    public override void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.ToString() + " is staying in trigger zone " + this.name);
+    }
+
+    public override void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.ToString() + " has left trigger zone " + this.name);
+    }
+}
