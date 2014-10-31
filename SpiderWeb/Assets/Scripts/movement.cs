10ba77c71ad3ab23f529d4063aca0e68273f04fe
Justin Bonczek <justin.bonczek@gmail.com>
2014-10-31 16:24:14 -0400

    Committing Kelsey's character work

---
+++ SpiderWeb/Assets/Scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    //Variables
+    float maxSpeed;
+    float maxStrength;
+    float maxJumpHeight;
+
+	// Use this for initialization
+	void Start () {
+        maxSpeed = 2;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * maxSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * maxSpeed * Time.deltaTime;
+        }
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.position += Vector3.up * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    transform.position += Vector3.down * Time.deltaTime;
+        //}
+
+	}
+}
