493a282547051a652dfd4392938b5fc2d9e60f73
ksw9906 <ksw9906@rit.edu>
2014-11-04 17:10:24 -0500

    Created the script for a wall to be destroyed if enough force is applied

---
+++ SpiderWeb/Assets/Scripts/wallCollide.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallCollide : MonoBehaviour {
+
+    private float stability = 10;
+    private float force;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Character"))
+        {
+            force = col.gameObject.GetComponent<CharacterVariable>().Force;
+            if (force > stability)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
