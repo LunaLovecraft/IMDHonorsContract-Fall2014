2671bd8e1c3e300d9497c4cf8eebf5d69a0b1809
kelsey weinman <ksw9906@rit.edu>
2014-11-09 13:51:52 -0500

    fluffs!!

---
+++ SpiderWeb/Assets/Scripts/FluffScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluffScript : MonoBehaviour {
+
+    public float desiredY;
+    public float lowestY;
+    public float floatForce;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (transform.position.y < lowestY)
+        {
+            rigidbody2D.AddForce(new Vector3(0, floatForce, 0));
+            //transform.position.Set(transform.position.x, transform.position.y + floatForce * Time.deltaTime, transform.position.z);
+        }
+	
+	}
+}
