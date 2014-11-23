f5c6d5e368be3ca9e9bba0591b2a16f950d502f5
kelsey weinman <ksw9906@rit.edu>
2014-11-23 14:44:39 -0500

    Parallax again

---
+++ SpiderWeb/Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+    private Vector3 prevCharacterPos;
+    public GameObject actor;
+    public float speed = 10;
+
+	void Start () 
+    {
+        prevCharacterPos = actor.transform.position;	
+	}
+	
+	void Update () 
+    {
+        Vector3 deltaCharPos = actor.transform.position - prevCharacterPos;
+        transform.position = new Vector3(transform.position.x - deltaCharPos.x /speed, transform.position.y - deltaCharPos.y / speed, transform.position.z);
+        prevCharacterPos = actor.transform.position;
+	}
+}
