79d93d9a3070f1a184a783bd0294be1850e2d304
awk11 <awk11@outlook.com>
2014-11-23 14:31:10 -0500

    Adams Update
    Added the first part of level 1, added a non stick tag for walls, and
    added a script that allows sprites to fade in and out when the player
    collides with them

---
+++ SpiderWeb/Assets/Scripts/TriggerZone Scripts/SeeThroughSprites.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Allows for a sprite to fade in/out depending on whether or 
+//not a player has collided with it
+public class SeeThroughSprites : AbstractTriggerZone {
+
+	//The sprite thay will disappear when the player enters its zone
+	public SpriteRenderer Overlay;
+
+	bool isIn = false;
+
+	public override void OnTriggerStay2D (Collider2D other)
+	{
+		isIn= true;
+		Overlay.material.color = new Color(Overlay.material.color.r, Overlay.material.color.g, Overlay.material.color.b, Mathf.Lerp(Overlay.material.color.a, 0, Time.deltaTime * 2));
+		//Overlay.enabled = false;
+	}
+
+	public override void OnTriggerExit2D(Collider2D other)
+	{
+		//Overlay.enabled = true;
+		isIn = false;
+	}
+
+	public override void OnTriggerEnter2D(Collider2D other)
+	{
+
+	}
+
+	public void Update()
+	{
+		if(!isIn)
+			Overlay.material.color = new Color(Overlay.material.color.r, Overlay.material.color.g, Overlay.material.color.b, Mathf.Lerp(Overlay.material.color.a, 1, Time.deltaTime * 2));
+
+	}
+}
