d09d3ea716f495292315bc1043c9b9924f9cc475
SarahBishop <GnomishInquisitor@gmail.com>
2014-12-01 16:31:46 -0500

    Tutorial Completed
    -The Tutorial level has been completed
    -A script that transitions between levels has been created
    -Currently, a StartScreen --> Tutorial --> TeseLevelSceen

---
+++ SpiderWeb/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	public Transform playerSpot;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Mathf.Abs (this.transform.position.x - playerSpot.transform.position.x) < 2)
+			Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
