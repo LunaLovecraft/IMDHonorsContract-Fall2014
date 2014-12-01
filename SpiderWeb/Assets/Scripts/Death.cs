2d53643f3adb6928b07d0aa2665748a7a7847a2a
kelsey weinman <ksw9906>
2014-12-01 17:43:18 -0500

    death and respawn

--- SpiderWeb/Assets/Scripts/Death.cs
+++ SpiderWeb/Assets/Scripts/Death.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class Death : MonoBehaviour {
 
-	// Use this for initialization
-    public void Death(Transform empty)
+    public Transform empty;
+    public void Die()
     {
         transform.position = empty.position;
     }
