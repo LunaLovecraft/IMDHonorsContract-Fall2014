9e4ff68f79166f05b76dafe23cfbcf8e02a0a3dd
kelsey weinman <ksw9906>
2014-12-01 16:36:55 -0500

    Death script

---
+++ SpiderWeb/Assets/Scripts/Death.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	// Use this for initialization
+    public void Death(Transform empty)
+    {
+        transform.position = empty.position;
+    }
+}
